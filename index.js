1bd3e26af6698b6f94397b4b1a11e963a6a5b1c7
rangermauve <RangerMauve@hotmail.com>
2017-04-21 17:38:05 -0400

    Rewrite with splitting topic into array

--- index.js
+++ index.js
@@ -4,106 +4,62 @@ var SINGLE = "+";
 var ALL = "#";
 
 module.exports = {
-	match: match,
+	matches: matches,
 	extract: extract,
 	exec: exec,
 };
 
 function exec(pattern, topic) {
-	return match(pattern, topic) ? extract(pattern, topic) : null;
+	return matches(pattern, topic) ? extract(pattern, topic) : null;
 }
 
-function match(pattern, topic) {
-	var patternLength = pattern.length;
-	var almostPatternLength = patternLength - 1;
-	var topicLength = topic.length;
-	var patternIndex = 0;
-	var topicIndex = 0;
+function matches(pattern, topic) {
+	var patternSegments = pattern.split(SEPARATOR);
+	var topicSegments = topic.split(SEPARATOR);
 
-	// Topics shouldn't contain wildcards!
-	if((topic.indexOf(SINGLE) !== -1) || (topic.indexOf(ALL) !== -1))
-		return false;
+	var patternLength = patternSegments.length;
+	var topicLength = topicSegments.length;
+	var lastIndex = patternLength - 1;
 
-	while(patternIndex < patternLength){
-		// Get the next pattern character to test against
-		var patternChar = pattern[patternIndex];
-		var atEnd = patternIndex === almostPatternLength;
+	for(var i = 0; i < patternLength; i++){
+		var currentPattern = patternSegments[i];
+		var patternChar = currentPattern[0];
+		var currentTopic = topicSegments[i];
 
-		if(patternChar === ALL){
-			// # wildcard must be at the end
-			if(patternLength === 1) return true;
-			return (toNext(pattern, patternIndex) === almostPatternLength);
-		} else if(patternChar === SINGLE) {
-			// If already at the end, finish up inhere
-			// Go to the next segment
-			patternIndex = toNext(pattern, patternIndex);
-			topicIndex = toNext(topic, topicIndex);
-			// If we're at the end, exit the loop
-			if(atEnd){
-				patternIndex++;
-				topicIndex++;
-			}
-		} else {
-			var topicChar = topic[topicIndex];
-			if(topicChar !== patternChar)
-				return false;
-			patternIndex++;
-			topicIndex++;
-		}
+		if(!currentTopic) return false;
+
+		// Only allow # at end
+		if(patternChar === ALL)
+			return i === lastIndex;
+		if(patternChar !== SINGLE && currentPattern !== currentTopic)
+			return false;
 	}
 
-	return (topicIndex === topicLength);
+	return patternLength === topicLength;
 }
 
 
 function extract(pattern, topic) {
-	var patternLength = pattern.length;
-	var almostPatternLength = patternLength - 1;
-	var patternIndex = 0;
-	var topicIndex = 0;
 	var params = {};
+	var patternSegments = pattern.split(SEPARATOR);
+	var topicSegments = topic.split(SEPARATOR);
 
-	var paramName;
-	var paramValue;
+	var patternLength = patternSegments.length;
 
-	while(patternIndex < patternLength){
-		var patternChar = pattern[patternIndex];
-		var nextChar = pattern[patternIndex + 1];
+	for(var i = 0; i < patternLength; i++){
+		var currentPattern = patternSegments[i];
+		var patternChar = currentPattern[0];
 
-		var nextPattern = toNext(pattern, patternIndex);
-		var nextTopic = toNext(topic,topicIndex);
+		if(currentPattern.length === 1)
+			continue;
 
-		var atEnd = nextPattern === almostPatternLength;
-
-		if(patternChar === ALL && nextChar) {
-			paramName = pattern.slice(patternIndex + 1);
-			paramValue = topic.slice(topicIndex).split("/");
-			params[paramName] = paramValue;
-		} else if(patternChar === SINGLE && nextChar !== "/") {
-			if(atEnd){
-				paramName = pattern.slice(patternIndex + 1);
-				paramValue = topic.slice(topicIndex);
-			} else {
-				paramName = pattern.slice(patternIndex + 1, nextPattern - 1);
-				paramValue = topic.slice(topicIndex, nextTopic - 1);
-			}
-			params[paramName] = paramValue;
-		}
-
-		patternIndex = nextPattern;
-		topicIndex = nextTopic;
-
-		if(atEnd) {
-			patternIndex++;
-			topicIndex++;
+		if(patternChar === ALL){
+			params[currentPattern.slice(1)] = topicSegments.slice(i);
+			break;
+		} else if(patternChar === SINGLE){
+			params[currentPattern.slice(1)] = topicSegments[i];
 		}
 	}
 
 	return params;
-}
-
-function toNext(topic, index) {
-	var nextIndex = topic.indexOf(SEPARATOR, index);
-	if(nextIndex === -1) return topic.length - 1;
-	else return nextIndex + 1;
 }
